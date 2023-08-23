b4facf8f2f1218debab89735c836c843a0ef18bb
Nishan Dhakal <nissandhakal11@gmail.com>
2023-08-23 20:18:10 +0545

    first commit

---
+++ RAW/virtual_destructor.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Base
+{
+    public:
+   // ~Base() non virtual
+    virtual ~Base()
+    {
+        cout<<"Base  destroyed"<<endl;
+    }
+    
+};
+class Derived : public Base
+{
+    public:
+    ~Derived()
+    {
+        cout<<"Derived Destoyed"<<endl;
+    }
+};
+int main()
+{
+    Base *p =new Derived;
+    delete p;
+    return 0;
+}
