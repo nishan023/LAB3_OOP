20f96971b4de91576db876cd0dd8ab612481cfd8
Nishan Dhakal <nissandhakal11@gmail.com>
2023-08-23 19:26:36 +0545

    first commit

---
+++ LAB5/4_virtual_base_class.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class A
+{
+    protected:
+    int adata;
+
+};
+class B : virtual public A {};
+class C: virtual public A{};
+class D : public B ,public C
+{
+    public:
+    D(int a)
+    {
+        adata=a;
+    }
+    int getdata()
+    {
+        return adata;
+    }
+};
+int main()
+{
+    D d(6);
+    cout<<d.getdata();
+    return 0;
+}
---
+++ LAB5/5_abstract_class_and_concrete_class.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class Polygon
+{
+    protected:
+    int width , height;
+    public :
+    void setvalues(int a,int b)
+    {
+        width=a;
+        height=b;
+
+    }
+    virtual int area(void)=0;
+};
+class Rectangle :public Polygon
+{
+    public:
+    int area(void)
+    {
+        return (width * height);
+    }
+};
+class Triangle : public Polygon
+{
+    public:
+    int area(void)
+    {
+        return (width * height/2);
+    }
+};
+int main()
+{
+    Rectangle r;
+    Triangle t;
+    Polygon *p;
+    p=&r;
+    p->setvalues(4,5);
+    cout<<"Area of rectangle="<<p->area()<<endl;
+    p=&t;
+    p->setvalues(6,4);
+    cout<<"Area of triangle ="<<p->area();
+    return 0;
+}
