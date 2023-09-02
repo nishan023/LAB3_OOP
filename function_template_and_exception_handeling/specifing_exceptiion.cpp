7a984340270eb29fce529b27b686216f47e73a20
Nishan Dhakal <nissandhakal11@gmail.com>
2023-09-02 21:26:53 +0545

    first commit

---
+++ function_template_and_exception_handeling/specifing_exceptiion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void test(int x) throw(int, double)
+{
+    if (x == 0)
+        throw x;
+    if (x == 1)
+        throw 1.0;
+}
+int main()
+{
+    try
+    {
+        test(1);
+    }
+    catch (int m)
+    {
+        cout << "Caught an integer." << endl;
+    }
+    catch (double d)
+    {
+        cout << "Caught a double.";
+    }
+    return 0;
+}
