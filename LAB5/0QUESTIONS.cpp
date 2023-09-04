3c414e7d1c1335e0ad362bf01dd0d4e2a1426596
Nishan Dhakal <nissandhakal11@gmail.com>
2023-09-04 17:41:37 +0545

    first commit

---
+++ LAB5/0QUESTIONS.cpp
@@ -0,0 +1,9 @@
+/*
+1. Write a program to show the use of virtual function.
+2. Write a program to show the use of abstract class and pure virtual function.
+3. Write a program to show the use of friend function and friend class.
+4. Write a program to show the use of virtual base class.
+5. Write a program to show the use of abstract class and concrete class.
+6. Write a program to show the use of static function members.
+7. Write a program to show the use of “this” pointer.
+*/
