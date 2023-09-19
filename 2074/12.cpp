dc8d58e0b24b3985d27651d57f2cbfa6d65e1fe2
Nishan Dhakal <nissandhakal11@gmail.com>
2023-09-19 18:56:30 +0545

    first  commit

---
+++ 2074/12.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter values of a and b:" << endl;
+    cin >> a >> b;
+    try
+    {
+        if (b == 0)
+            throw b;
+        else
+            cout << "Result =" << (float)a / b;
+    }
+
+    catch (int i)
+    {
+        cout << "Dvide by zero exception:b=" << i;
+    }
+    cout << endl
+         << "END";
+    return 0;
+}
--- 2074/9.cpp
+++ 2074/9.cpp
@@ -1,7 +1,7 @@
 
 #include <iostream>
 using namespace std;
-class A  //abstract class
+class A // abstract class
 {
 protected:
     int data;
